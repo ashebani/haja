d7f035c5c968081d4fb004ea7e309712622259c6
ashebani <azizoo2001.as@gmail.com>
2022-04-20 22:43:42 +0200

    Read subjects from an Excel sheet

--- haja.py
+++ haja.py
@@ -1,7 +1,7 @@
-
 from random import randint
 from string import ascii_uppercase 
 import pandas as pd
+import itertools
 
 global characters
 characters = ascii_uppercase + "0123456789"
@@ -14,35 +14,67 @@ def random_str(length):
         rstr += char
     return rstr
 
+
+class TimeSlot:
+
+    def __init__(self, day, period):
+        self.day = day
+        self.period = period
+
+
 class Subject:
     
-    def __init__(self, code, name):
+    def __init__(self, code, name, group, time_slots):
         self.code = code
         self.name = name
+        self.group = group
+        self.time_slots = time_slots
+
+
 
 # Print pandas dataframe
 def print_full_table(data):
+
     table = pd.DataFrame(data, index=['Monday', 'Tuesday', 'Wednsday'])
     for i in range(len(table)):
         for b in range(len(table)):
-            classs = table.iloc[i, b]
-            table.iloc[i, b] = f"{classs.code} - {classs.name}"
+            class_ = table.iloc[i, b]
+            table.iloc[i, b] = f"{class_.code} - {class_.name}"
 
     table.rename({'Monday':1,"Tuesday":2,"Wednesday":3}, axis = 1, inplace=True)
     print(table.to_string())
 
-time_table = {
-   "Monday": [],
-   "Tuesday": [],
-   "Wednesday": []
-}
 
-for day in time_table.keys():
-    for period in range(3):
-        classs = Subject(random_str(6), random_str(15))
-        time_table[day].append(classs)
+if __name__ == "__main__":
+
+    time_table = {
+       "Monday": [],
+       "Tuesday": [],
+       "Wednesday": []
+    }
 
 
+    subjects = {}
+    data = pd.read_excel('dataset.xlsx',sheet_name = 'Sheet2')
+    data.set_index('days', inplace=True)
 
+    days_index = list(data.index)
 
-print_full_table(time_table)
+    for i in range(len(data)):
+        for j in range(len(data.columns)):
+            cell = data.iloc[i, j]
+            time_slot = TimeSlot(days_index[i], j)
+
+            if type(cell) is not str: continue
+
+            if cell not in subjects.keys():
+                code = cell[0:7]
+                group = cell[8]
+
+                subjects[cell] = Subject(code, "", group, [time_slot])
+
+            else:
+                subjects[cell].time_slots.append(time_slot)
+
+                [[],[],[],[]]
+
